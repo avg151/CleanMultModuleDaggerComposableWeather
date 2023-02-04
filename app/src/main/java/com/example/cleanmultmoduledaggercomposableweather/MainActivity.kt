7c8d4980f01042a60ba1ba6c7dfbec82a170eb11
Vladimir Karas <avg151@ya.ru>
2023-02-05 01:23:37 +0400

    ds

--- app/src/main/java/com/example/cleanmultmoduledaggercomposableweather/MainActivity.kt
+++ app/src/main/java/com/example/cleanmultmoduledaggercomposableweather/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cleanmultmoduledaggercomposableweather.ui.theme.CleanMultModuleDaggerComposableWeatherTheme
+import com.example.core_ds.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +28,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
